cbadb1748b36d858a943e0be9fbfdcb14e0945c1
jvcodes <jagadeeshtv1995@gmail.com>
2026-07-01 20:05:50 +0530

    Prepared Django for Render.com deployment

--- sitehub/settings.py
+++ sitehub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-38*_7m6)w+h^3(li4w)+^-5e!n6oi4)#is-ppx+^v+!fk^pg0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
      "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
